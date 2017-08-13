bcc8f08dfa75dd2e869604e59f06ed4b49ca907a
Sergei <sergeiws11@gmail.com>
2017-08-13 15:05:46 +0300

    Added static

--- Project/settings.py
+++ Project/settings.py
@@ -16,6 +16,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "navalny_live/static"),
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
--- navalny_live/urls.py
+++ navalny_live/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from navalny_live.views import (
     ListShowsView, PushSubscribe, PushUnsubscribe, RetrieveShowView,
@@ -57,4 +59,6 @@ urlpatterns = [
             )
         ])
     )
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
